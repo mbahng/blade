11685dde86a5bd91701b133e50301c2d9eb95107
Muchang Bahng <bahngmc@gmail.com>
2024-11-09 23:53:36 -0500

    added merkle tree features

--- block/block.js
+++ block/block.js
@@ -1,6 +1,16 @@
 import assert from "assert/strict";
 import { Hex } from "../utils/bytestream.js";
 import { Transaction } from "../transactions/transactions.js";
+import { sha256 } from "../utils/hash.js";
+
+export class MerkleNode {
+  constructor(value, left = null, right = null) {
+    assert(value instanceof Hex);
+    this.value = value; 
+    this.left = left; 
+    this.right = right; 
+  }
+}
 
 export class Block {
   constructor(prev_block_hash, transactions) {
@@ -11,5 +21,50 @@ export class Block {
     this.prev_block_hash = prev_block_hash; 
     this.txs = transactions; 
     this.timestamp = Date.now(); 
+
+    // merkle tree needed for SPV nodes
+    this.merkletree = this.create_merkle_tree(); 
   }
+
+  create_merkle_tree() {
+    if (this.txs.length === 0) {
+      return null;
+    }
+
+    // Create leaf nodes from transaction hashes
+    let leaves = this.txs.map(tx => new MerkleNode(tx.id));
+    return this.build_tree_level(leaves);
+  }
+
+  build_tree_level(nodes) {
+    // If we have only one node, it's the root
+    if (nodes.length === 1) {
+      return nodes[0];
+    }
+
+    const new_level = [];
+
+    // Process nodes in pairs
+    for (let i = 0; i < nodes.length; i += 2) {
+      const left = nodes[i];
+      // If we have an odd number of nodes, duplicate the last one
+      const right = i + 1 < nodes.length ? nodes[i + 1] : nodes[i];
+
+      // Combine the hashes of the children to create parent hash
+      const combined_hash = sha256(left.value.concat(right.value));
+      
+      // Create new parent node
+      const parent = new MerkleNode(combined_hash, left, right);
+      new_level.push(parent);
+    }
+
+    // Recursively build the next level
+    return this.build_tree_level(new_level);
+  }
+
+  containsTxHash(tx_hash) {
+    assert(tx_hash instanceof Hex); 
+    return false; 
+  }
+
 }
--- main.js
+++ main.js
@@ -8,20 +8,25 @@ import { TransactionInput, TransactionOutput, Transaction } from "./transactions
 let alice = secp256k1(true); 
 let bob = secp256k1(true); 
 
-let sig = alice.private.create_signature("hello"); 
-console.log(sig);
-console.log(sig.verify());
+let txs = []; 
 
-// let tx_i = new TransactionInput(
-//    Hex.random(256), 
-//    1n, 
-//    Hex.random(256)
-//  ); 
-// let tx_o = new TransactionOutput(
-//   10n, 
-//   alice.public
-// )
-//
-// let tx = new Transaction([tx_i], [tx_o]); 
-//
-// console.log(tx); 
+for (let i = 0; i < 4; i++) {
+  let tx_i = new TransactionInput(
+     Hex.random(256), 
+     1n, 
+     Hex.random(256)
+   ); 
+  let tx_o = new TransactionOutput(
+    10n, 
+    alice.public
+  )
+
+  let tx = new Transaction([tx_i], [tx_o]); 
+  txs.push(tx);
+}
+
+let prev_block_hash = Hex.random(256); 
+
+let block = new Block(prev_block_hash, txs); 
+
+console.log(block);
